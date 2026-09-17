cb65eb73d415d7266be165e2054f496bbe653424
eroxyi <eyu6257@gmail.com>
2026-09-17 08:54:12 -0400

    added a read function, replaced the MNIST dataset with an easire to parse data file

--- src/read.c
+++ src/read.c
@@ -1,28 +1,68 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct {
-  float **input;
-  int *labels;
+#define NUM_IMAGE 1000
+#define PIXELS 784
+
+typedef struct Dataset {
+  unsigned char **images;
+  int *label;
   int n;
 } Dataset;
 
-Dataset *parse(const char *path, const char *path2) {
-  Dataset *set = (Dataset *)(malloc(sizeof(Dataset)));
-  FILE *file_ptr;
+Dataset *parse(const char *dir) {
+  Dataset *temp = (malloc(sizeof(Dataset)));
 
-  if ((file_ptr = fopen(path, "rb")) == NULL) {
-    printf("File reading failed!\n");
-    exit(1);
+  temp->images = (malloc(NUM_IMAGE * sizeof(char *)));
+  temp->label = (malloc(sizeof(int) * NUM_IMAGE));
+  temp->n = NUM_IMAGE;
+
+  DIR *dirFile = opendir(dir);
+  struct dirent *hFile;
+
+  int counter = 0;
+  while ((hFile = readdir(dirFile)) != NULL) {
+    if (!strcmp(hFile->d_name, "."))
+      continue;
+    if (!strcmp(hFile->d_name, ".."))
+      continue;
+
+    char fullPath[250];
+    strcpy(fullPath, "../data/");
+    strcat(fullPath, hFile->d_name);
+
+    FILE *fptr = fopen(fullPath, "rb");
+
+    temp->images[counter] = malloc(NUM_IMAGE * sizeof(char) * PIXELS);
+    fread(temp->images[counter], sizeof(char) * PIXELS, NUM_IMAGE, fptr);
+
+    temp->label[counter] = hFile->d_name[4];
+
+    counter++;
   }
 
-  fseek(file_ptr, 4, SEEK_SET);
-  fread(&(set->n), 4, 1, file_ptr);
-  fseek(file_ptr, 16, SEEK_SET);
+  return temp;
+}
 
-  // loop through and populate input
+void visualize(Dataset *s) {
+  char symbols[] = ".-+*#";
+  printf("This should be outputting the number %d", *(s->label + 1) - '0');
 
-  // loop through and populate label
+  for (int i = 0; i < 28; i++) {
+    for (int j = 1; j < 28; j++) {
+      if (s->images[1][28 * i + j] > 128) {
+        printf("#");
+      } else {
+        printf(" ");
+      }
+    }
+    printf("\n");
+  }
+}
 
-  fclose(file_ptr);
+int main() {
+  Dataset *set = parse("../data/");
+  visualize(set);
 }
