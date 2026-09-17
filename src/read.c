061d05f14e8c7b0a78c75e5d65fa86062fe20338
eroxyi <eyu6257@gmail.com>
2026-09-17 09:36:35 -0400

    fixed bugs in read.c

--- src/read.c
+++ src/read.c
@@ -5,6 +5,7 @@
 
 #define NUM_IMAGE 1000
 #define PIXELS 784
+#define MAX_FILES 10
 
 typedef struct Dataset {
   unsigned char **images;
@@ -15,8 +16,8 @@ typedef struct Dataset {
 Dataset *parse(const char *dir) {
   Dataset *temp = (malloc(sizeof(Dataset)));
 
-  temp->images = (malloc(NUM_IMAGE * sizeof(char *)));
-  temp->label = (malloc(sizeof(int) * NUM_IMAGE));
+  temp->images = (malloc(NUM_IMAGE * sizeof(char *) * MAX_FILES));
+  temp->label = (malloc(sizeof(int) * NUM_IMAGE * MAX_FILES));
   temp->n = NUM_IMAGE;
 
   DIR *dirFile = opendir(dir);
@@ -30,17 +31,19 @@ Dataset *parse(const char *dir) {
       continue;
 
     char fullPath[250];
-    strcpy(fullPath, "../data/");
+    strcpy(fullPath, dir);
     strcat(fullPath, hFile->d_name);
 
     FILE *fptr = fopen(fullPath, "rb");
 
-    temp->images[counter] = malloc(NUM_IMAGE * sizeof(char) * PIXELS);
-    fread(temp->images[counter], sizeof(char) * PIXELS, NUM_IMAGE, fptr);
+    for (int i = 0; i < NUM_IMAGE; i++) {
+      temp->images[counter] = malloc(sizeof(char) * PIXELS);
+      fread(temp->images[counter], sizeof(char) * PIXELS, 1, fptr);
+      temp->label[counter] = hFile->d_name[4];
+      counter++;
+    }
 
-    temp->label[counter] = hFile->d_name[4];
-
-    counter++;
+    fclose(fptr);
   }
 
   return temp;
@@ -51,7 +54,7 @@ void visualize(Dataset *s) {
   printf("This should be outputting the number %d", *(s->label + 1) - '0');
 
   for (int i = 0; i < 28; i++) {
-    for (int j = 1; j < 28; j++) {
+    for (int j = 0; j < 28; j++) {
       if (s->images[1][28 * i + j] > 128) {
         printf("#");
       } else {
